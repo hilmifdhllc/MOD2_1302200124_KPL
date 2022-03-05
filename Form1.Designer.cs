c875c25f9398b5cab6e6093590882ae4833af6cb
Hilmi FC <hilmie123013@gmail.com>
2022-03-05 14:22:22 +0700

    modul2_1302200124

---
+++ Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace modul2_1302200124
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textDisplay1 = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btnTambah = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btnSamaDengan = new System.Windows.Forms.Button();
+            this.textDisplay2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textDisplay1
+            // 
+            this.textDisplay1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textDisplay1.Location = new System.Drawing.Point(12, 22);
+            this.textDisplay1.Multiline = true;
+            this.textDisplay1.Name = "textDisplay1";
+            this.textDisplay1.Size = new System.Drawing.Size(416, 58);
+            this.textDisplay1.TabIndex = 0;
+            this.textDisplay1.Text = "0";
+            this.textDisplay1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(35, 104);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(76, 54);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(35, 164);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(76, 54);
+            this.btn4.TabIndex = 4;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(35, 224);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(76, 54);
+            this.btn7.TabIndex = 7;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.Location = new System.Drawing.Point(35, 284);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(76, 54);
+            this.btnTambah.TabIndex = 10;
+            this.btnTambah.Text = "+";
+            this.btnTambah.UseVisualStyleBackColor = true;
+            this.btnTambah.Click += new System.EventHandler(this.btnTambah_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(157, 104);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(76, 54);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(157, 164);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(76, 54);
+            this.btn5.TabIndex = 5;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(157, 224);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(76, 54);
+            this.btn8.TabIndex = 8;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(157, 284);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(76, 54);
+            this.btn0.TabIndex = 0;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(278, 104);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(76, 54);
+            this.btn3.TabIndex = 3;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(278, 164);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(76, 54);
+            this.btn6.TabIndex = 6;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(278, 224);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(76, 54);
+            this.btn9.TabIndex = 9;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btnSamaDengan
+            // 
+            this.btnSamaDengan.Location = new System.Drawing.Point(278, 284);
+            this.btnSamaDengan.Name = "btnSamaDengan";
+            this.btnSamaDengan.Size = new System.Drawing.Size(76, 54);
+            this.btnSamaDengan.TabIndex = 12;
+            this.btnSamaDengan.Text = "=";
+            this.btnSamaDengan.UseVisualStyleBackColor = true;
+            this.btnSamaDengan.Click += new System.EventHandler(this.btnSamaDengan_Click);
+            // 
+            // textDisplay2
+            // 
+            this.textDisplay2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textDisplay2.Location = new System.Drawing.Point(35, 44);
+            this.textDisplay2.Name = "textDisplay2";
+            this.textDisplay2.Size = new System.Drawing.Size(47, 15);
+            this.textDisplay2.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 369);
+            this.Controls.Add(this.textDisplay2);
+            this.Controls.Add(this.btnSamaDengan);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btnTambah);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.textDisplay1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textDisplay1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btnTambah;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btnSamaDengan;
+        private System.Windows.Forms.TextBox textDisplay2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace modul2_1302200124
+{
+    public partial class Form1 : Form
+    {
+        decimal bil1;
+        decimal bil2;
+        int opr;
+        Boolean opr_done = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textDisplay1.Text == "0")
+            {
+                textDisplay1.Text = "1";
+            }
+            else
+            {
+                textDisplay1.Text += "1";
+            }
+
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (textDisplay1.Text == "0")
+            {
+                textDisplay1.Text = "2";
+            }
+            else
+            {
+                textDisplay1.Text += "2";
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (textDisplay1.Text == "0")
+            {
+                textDisplay1.Text = "3";
+            }
+            else
+            {
+                textDisplay1.Text += "3";
+            }
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (textDisplay1.Text == "0")
+            {
+                textDisplay1.Text = "4";
+            }
+            else
+            {
+                textDisplay1.Text += "4";
+            }
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (textDisplay1.Text == "0")
+            {
+                textDisplay1.Text = "5";
+            }
+            else
+            {
+                textDisplay1.Text += "5";
+            }
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (textDisplay1.Text == "0")
+            {
+                textDisplay1.Text = "6";
+            }
+            else
+            {
+                textDisplay1.Text += "6";
+            }
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            if (textDisplay1.Text == "0")
+            {
+                textDisplay1.Text = "7";
+            }
+            else
+            {
+                textDisplay1.Text += "7";
+            }
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (textDisplay1.Text == "0")
+            {
+                textDisplay1.Text = "8";
+            }
+            else
+            {
+                textDisplay1.Text += "8";
+            }
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            if (textDisplay1.Text == "0")
+            {
+                textDisplay1.Text = "9";
+            }
+            else
+            {
+                textDisplay1.Text += "9";
+            }
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            if(textDisplay1.Text != "0")
+            {
+                textDisplay1.Text += "0";
+            }
+        }
+
+        private void btnTambah_Click(object sender, EventArgs e)
+        {
+            bil1 = Convert.ToDecimal(textDisplay1.Text);
+            textDisplay2.Text = "+";
+            textDisplay1.Text = " ";
+            opr = 1;
+            opr_done = true;
+
+        }
+
+        private void btnSamaDengan_Click(object sender, EventArgs e)
+        {
+            if (opr_done == true)
+                bil2 = Convert.ToDecimal(textDisplay1.Text);
+            {
+                switch (opr)
+                {
+                    case 1:
+                        textDisplay1.Text = Convert.ToString(bil1 + bil2);
+                        break;
+                }
+                opr_done = false;
+            }
+        }
+    }
+}
